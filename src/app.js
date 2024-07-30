8ab9a3c8d0e2c8098b16561b160f3954a3ea523c
Katie Loesch <catherine.loesch@protonmail.com>
2024-07-30 22:35:58 +0100

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const cors = require("cors");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env().PORT || 3000;
 
 // var corsOptions = {
 //   origin: "http://localhost:3000",
@@ -16,34 +17,34 @@ const app = express();
 app.use(cors());
 // Proxy configuration
 app.use(
-  "/api",
+  '/api',
   createProxyMiddleware({
-    target: "http://puzzle.mead.io/puzzle",
+    target: 'http://puzzle.mead.io/puzzle',
     changeOrigin: true,
     pathRewrite: {
-      "^/api": "", // Remove the /api/puzzle prefix when forwarding
+      '^/api': '', // Remove the /api/puzzle prefix when forwarding
     },
   })
 );
 
 //define paths for express config
-const publicDirPath = path.join(__dirname, "../public");
+const publicDirPath = path.join(__dirname, '../public');
 // const viewsPath = path.join(__dirname, "../views");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // setup handlebars engine and views location
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 //setup static  directory to serve
 app.use(express.static(publicDirPath));
 
-app.get("/weather", (req, res) => {
+app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: "you must provide an address",
+      error: 'you must provide an address',
     });
   }
 
@@ -68,10 +69,10 @@ app.get("/weather", (req, res) => {
   );
 });
 
-app.get("/products", (req, res) => {
+app.get('/products', (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: "you must provide a search term",
+      error: 'you must provide a search term',
     });
   }
   console.log(req.query);
@@ -80,48 +81,44 @@ app.get("/products", (req, res) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Weather App",
-    name: "Hannah",
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Katie',
   });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About Me",
-    name: "Hannah",
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Katie',
   });
 });
 
-app.get("/help", (req, res) => {
-  res.render("help", {
-    title: "Help",
-    name: "Hannah",
-    msg: "SOS",
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Katie',
+    msg: 'SOS',
   });
 });
 
-app.get("/help/*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Hannah",
-    msg: "Help Article Found",
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Katie',
+    msg: 'Help Article Found',
   });
 });
 
-app.get("*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Hannah",
-    msg: "Page Not Found",
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Katie',
+    msg: 'Page Not Found',
   });
 });
 
-// app.get("", (req, res) => {
-//   res.send("<h1>weather content</h1>");
-// });
-
-app.listen(3000, () => {
-  console.log("server is up on port 3000...");
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
 });
