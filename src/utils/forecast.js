e776f32e6f54daba22ebb0059f0b0ea8d557fc0b
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-10 22:40:08 +0100

    change unit of fetched weatherstack data to metric (instead of fahrenheit temperature)
    close #1

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,26 @@
-const request = require("postman-request");
+const request = require('postman-request');
 
 const forecast = (latitude, longitude, cb) => {
   const weatherApi = {
-    url: "http://api.weatherstack.com/",
-    key: "c703d7a73d02bf90d8e94cebba5cd12c",
+    url: 'http://api.weatherstack.com/',
+    key: 'c703d7a73d02bf90d8e94cebba5cd12c',
   };
 
-  const url = `${weatherApi.url}current?access_key=${weatherApi.key}&query=${latitude},${longitude}&units=f`;
+  const url = `${weatherApi.url}current?access_key=${weatherApi.key}&query=${latitude},${longitude}&units=m`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
       // low level error handling
-      cb("unable to connect to weather service!", undefined);
+      cb('unable to connect to weather service!', undefined);
     } else if (body.error) {
       // error handling for no matching results
-      cb("Unable to find location", undefined);
+      cb('Unable to find location', undefined);
     } else {
       const current = body.current;
+
       cb(
         undefined,
-        `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees.`
+        `${current.weather_descriptions[0]}- ${current.temperature}°C   | cloud cover: ${current.cloudcover}%   |   humidity: ${current.humidity}%   |   wind speed: ${current.wind_speed} km/h`
       );
     }
   });
